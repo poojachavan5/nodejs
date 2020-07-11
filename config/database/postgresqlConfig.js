454805f210cb183949104fc9d743e18a273acb72
poojachavan5 <48662107+poojachavan5@users.noreply.github.com>
2020-07-11 13:54:01 +0530

    Add files via upload

--- config/database/postgresqlConfig.js
+++ config/database/postgresqlConfig.js
@@ -7,9 +7,54 @@ const client = new Client({
     database: "pooja"
 })
 
-client.connect()
-.then(() => console.log("Connected Successfully"))
-.then(() => client.query("select * from Student_answers"))
-.then(results => console.table(results.rows))
-.catch(e => console.log(e))
-.finally(() => client.end())
+
+const axios= require('axios')
+function removeUnknown(e){
+    return e !== 'Unknown';
+}
+
+exports.question= async(req, res, next)=> {
+    try {
+
+        const index= 4
+        const question_id = 5
+        const question_code= 555
+        const question_text="Is java and javascript the same?"
+
+        
+
+        res.status(200).json({
+            status: true,
+            message: 'Questions',
+            index,
+            question_id,
+            question_code,
+            question_text
+        })
+    } catch (err) {
+        next(err);
+    }
+}
+
+const {Pool }= require('pg')
+const pool=new Pool({
+    user: "postgres",
+    password: "Pooja1999",
+    host: "localhost",
+    port: 5432,
+    database: "pooja"
+})
+
+pool.query('SELECT * from question', (err, res)=>{
+    try{
+        console.log("PostgreSQL Database Connected")
+        pool.end()
+        console.log(res.rows)
+        
+    }
+    catch (err)
+    {
+        console.log("Error in Connecting to PostgreSQL Database", err.stack)
+    }
+
+})
